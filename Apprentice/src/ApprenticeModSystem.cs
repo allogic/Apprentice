33d4dd033e522d8583272544756f9b2f5b4eae85
allogic <mial.burg@gmail.com>
2026-07-21 01:39:47 +0200

    ehh..

--- Apprentice/src/ApprenticeModSystem.cs
+++ Apprentice/src/ApprenticeModSystem.cs
@@ -1,4 +1,4 @@
-using Apprentice.src.weapons;
+using Apprentice.Weapon;
 using HarmonyLib;
 using System;
 using System.Linq;
--- Apprentice/src/weapons/Uchigatana.cs
+++ Apprentice/src/weapons/Uchigatana.cs
@@ -1,21 +1,20 @@
-﻿using System;
+﻿using Cairo;
+using System;
 using System.Collections.Generic;
-
+using System.Runtime.InteropServices;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
 using Vintagestory.API.MathTools;
 using Vintagestory.GameContent;
 
-namespace Apprentice.src.weapons
+namespace Apprentice.Weapon
 {
-	internal class Uchigatana
+	internal class AnimationStuff
 	{
-		public float deltaAcc = 0.0F;
-
-		public Uchigatana(ICoreClientAPI capi, long entityId)
+		private static void AnimationStuffHandling(ICoreClientAPI api, long entityId)
 		{
-			Entity? entity = capi.World.GetEntityById(entityId);
+			Entity? entity = api.World.GetEntityById(entityId);
 			EntityShapeRenderer? renderer = entity?.Properties.Client.Renderer as EntityShapeRenderer;
 
 			IAnimationManager? animManager = entity?.AnimManager;
@@ -28,18 +27,11 @@ namespace Apprentice.src.weapons
 
 			IAnimator? animator = animManager?.Animator;
 
-			ElementPose elemPose =  animator.GetPosebyName("Root");
+			// ElementPose elemPose = animator?.GetPosebyName("Root");
 
-			// deltaAcc += 0.05F;
-			// elemPose.translateY += (float)Math.Sin(deltaAcc) * 10.0F;
-			// if (deltaAcc > Math.Tau)
-			// {
-			// 	deltaAcc = 0.0F;
-			// }
+			// animator?.CalculateMatrices = true;
 
-			animator.CalculateMatrices = true;
-
-			AttachmentPointAndPose? rootPose = animator.GetAttachmentPointPose("Root");
+			// AttachmentPointAndPose? rootPose = animator.GetAttachmentPointPose("Root");
 
 			// rootPose.
 
@@ -49,26 +41,116 @@ namespace Apprentice.src.weapons
 		}
 	}
 
+	internal class DebugLineRenderer : IRenderer
+	{
+		private readonly IClientEventAPI eventApi;
+		private readonly IRenderAPI renderApi;
+
+		public double RenderOrder => 1.0;
+		public int RenderRange => 10;
+
+		private MeshData? mesh = null;
+		private MeshRef? meshRef = null;
+
+		public DebugLineRenderer(ICoreClientAPI api, int numLines)
+		{
+			eventApi = api.Event;
+			renderApi = api.Render;
+
+			mesh = new(numLines * 2, numLines * 2, false, false, true, false);
+			mesh?.mode = EnumDrawMode.Lines;
+
+			api.Event.RegisterRenderer(this, EnumRenderStage.Done);
+		}
+
+		public void OnRenderFrame(float deltaTime, EnumRenderStage stage)
+		{
+			if (stage != EnumRenderStage.Done) return;
+
+			if (meshRef != null)
+			{
+				IShaderProgram program = renderApi.CurrentActiveShader;
+
+				renderApi.GlDisableCullFace();
+				renderApi.RenderMesh(meshRef);
+				renderApi.GlEnableCullFace();
+			}
+		}
+
+		public void AddLine(
+			float x0, float y0, float z0,
+			float x1, float y1, float z1,
+			int color)
+		{
+			if (mesh == null) return;
+
+			mesh.AddVertexSkipTex(x0, y0, z0, color);
+			mesh.AddVertexSkipTex(x1, y1, z1, color);
+			mesh.AddIndex(0);
+			mesh.AddIndex(1);
+		}
+		public void AddCircle(
+			float x, float y, float z,
+			float radius, int numSegments, int color)
+		{
+			// TODO
+		}
+
+		public void Reset()
+		{
+			if (mesh == null) return;
+
+			mesh.Clear();
+		}
+		public void Commit()
+		{
+			if (mesh == null) return;
+
+			meshRef?.Dispose();
+			meshRef = renderApi.UploadMesh(mesh);
+		}
+		public void Dispose()
+		{
+			eventApi.UnregisterRenderer(this, EnumRenderStage.AfterFinalComposition);
+
+			meshRef?.Dispose();
+		}
+	}
+
 	internal class UchigatanaDashBehaviour : EntityBehavior
 	{
+		private readonly ICoreClientAPI clientApi;
+		private readonly IInputAPI inputApi;
+
 		EntityPlayer? entityPlayer = null;
+		DebugLineRenderer? lineRenderer = null;
 
 		private bool Init { get; set; }
 		private bool Playing { get; set; }
 
-		private float Duration { get; set; } = 1.0F;
-		private float Attenuator { get; set; } = 50.0F;
-		private float Dampening { get; set; } = 1.0F;
-		private float EngageAngle { get; set; } = 0.01745329251994329547F * 30.0F;
+		private float AnimationDuration { get; set; } = 4.0F;
+		private float AnimationStep { get; set; } = 0.2F;
+		private float DashImpulse { get; set; } = 0.3F;
 
-		private Vec3f Velocity { get; set; } = new();
+		// private float Duration { get; set; } = 1.0F;
+		// private float Attenuator { get; set; } = 5.0F;
+		// private float Dampening { get; set; } = 0.002F;
+		// private float EngageAngle { get; set; } = 0.01745329251994329547F * 30.0F;
 
-		public UchigatanaDashBehaviour(ICoreClientAPI capi, Entity entity) : base(entity)
+		private float AnimationTime { get; set; } = 0.0F;
+		private float GroundEpsilon { get; set; } = 0.0F;
+
+		public UchigatanaDashBehaviour(ICoreClientAPI api, Entity entity) : base(entity)
 		{
-			entityPlayer = capi.World.Player.Entity;
+			clientApi = api;
+			inputApi = api.Input;
 
-			capi.Input.RegisterHotKey("play_blood_scythe_anim", "Play a test sequence", GlKeys.F, HotkeyType.MovementControls);
-			capi.Input.SetHotKeyHandler("play_blood_scythe_anim", OnReset);
+			entityPlayer = api.World.Player.Entity;
+
+			lineRenderer = new(api, 1000);
+
+			inputApi.RegisterHotKey("play_blood_scythe_anim", "Play a test sequence", GlKeys.F, HotkeyType.MovementControls);
+			inputApi.SetHotKeyHandler("play_blood_scythe_anim", OnReset);
 		}
 
 		public override string PropertyName()
@@ -79,8 +161,8 @@ namespace Apprentice.src.weapons
 		{
 			if (entityPlayer == null) return;
 
-			EntityPos position = entityPlayer.Pos;
-
+			EntityPos transform = entityPlayer.Pos;
+			Vec3d forward = Vec3d.Zero;
 			float initalAngle = 0.0F;
 
 			if (Playing)
@@ -89,40 +171,67 @@ namespace Apprentice.src.weapons
 				{
 					Init = false;
 
-					initalAngle = position.Pitch;
+					AnimationTime = AnimationDuration;
+
+					initalAngle = transform.Pitch;
+					// forward = new Vec3d(GameMath.Sin(transform.Yaw), 0, GameMath.Cos(transform.Yaw)).Normalize();
+					forward = transform.GetViewVector().ToVec3d();
+
+					transform.SetPos(transform.X, transform.Y + GroundEpsilon, transform.Z);
+					transform.Motion.Set(0.0F, transform.Motion.Y, 0.0F);
+
+					if (lineRenderer != null)
+					{
+						lineRenderer.Reset();
+						lineRenderer.AddLine(
+							(float)transform.X, (float)transform.Y, (float)transform.Z,
+							(float)transform.Motion.X, (float)transform.Motion.Y, (float)transform.Motion.Z,
+							ColorUtil.ToRgba(0xFF, 0xFF, 0x00, 0x00));
+						lineRenderer.Commit();
+					}
+
+					// BlockSelection? blockSelection = null;
+					// EntitySelection? entitySelection = null;
+
+					// entity.World.RayTraceForSelection(entity.Pos.XYZ, target, ref blockSelection, ref entitySelection);
 
 					// entity.StartAnimation(""); // TODO
 					// entity.StopAnimation..
 				}
 
-				Vec3f forward = position.GetViewVector();
+				// transform.Add(Velocity);
+				// transform.SetPos(transform.X, transform.Y, transform.Z);
+				// transform.SetAngles(transform.Roll, transform.Yaw, transform.Pitch + EngageAngle);
 
-				Velocity += forward * Attenuator * deltaTime;
+				transform.Motion.Add(forward * DashImpulse);
 
-				position.Add(Velocity);
+				AnimationTime -= AnimationStep;
 
-				if (Velocity.Length() > 0.001F)
+				if (0.0F > AnimationTime)
 				{
-					position.SetAngles(position.Roll, position.Yaw, position.Pitch - EngageAngle);
+					AnimationTime = AnimationDuration;
 
-					Velocity -= Velocity * Dampening;
+					// transform.Motion.Set(0.0F, transform.Y, 0.0F);
 
 					Playing = false;
 					Init = true;
 				}
-
-				position.SetPos(position.X, position.Y, position.Z);
-				position.SetAngles(position.Roll, position.Yaw, position.Pitch + EngageAngle);
 			}
 
+			// TODO: check out these flags..
 			// entity.ApplyGravity
-			// entity.requirePosesOnServer // TODO: check this flag..
+			// entity.requirePosesOnServer
 		}
 
 		private bool OnReset(KeyCombination combination)
 		{
-			Playing = true;
-			Init = true;
+			// TODO: add reset timer..
+
+			if (Playing == false)
+			{
+				Playing = true;
+				Init = true;
+			}
 
 			return true;
 		}
